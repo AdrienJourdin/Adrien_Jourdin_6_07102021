9e43430648ba614234241216581600cb9b5a163e
Adrien Jourdin <adrjourdin@gmail.com>
2021-11-02 14:27:29 +0100

    Ajout du middleware umter à la route put

--- routes/sauce.js
+++ routes/sauce.js
@@ -16,7 +16,7 @@ const multer = require('../middleware/multer-config');
   router.post('/',auth, multer,sauceCtrl.createSauce);
   router.get('/',auth,sauceCtrl.getAllSauces);
   router.get('/:id', auth, sauceCtrl.getOneSauce);
-  router.put('/:id', auth, sauceCtrl.modifySauce);
+  router.put('/:id', auth,multer, sauceCtrl.modifySauce);
   router.delete('/:id',  auth,sauceCtrl.deleteSauce);
   router.post('/:id/like',auth,sauceCtrl.postLike);
   //Export de la route
